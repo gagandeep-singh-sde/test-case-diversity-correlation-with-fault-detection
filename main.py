662b3b4cba24124f9b678adbd633bcf3a645aa81
Gagandeep Singh <gagandeep.singh.sde@gmail.com>
2023-12-21 15:33:15 -0500

    normalization added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import subprocess
 
 import pandas as pd
 from scipy.stats import spearmanr
+from sklearn.preprocessing import MinMaxScaler
 
 import sys
 import numpy
@@ -35,20 +36,17 @@ def create_dc_report(state_machine_folder):
 def calculate_diversity(state_machine_path, data_representation, diversity_metric, aggregation_method):
     fsm_test_suites_path = os.path.join(state_machine_path, 'FSM_test_suites')
 
-    # Check if 'FSM_test_suites' folder exists
     if os.path.exists(fsm_test_suites_path):
         print(f"Found 'FSM_test_suites' folder in {state_machine_path}")
 
         test_suite_files = []
         diversity_values = []
 
-        # Iterate over each txt file in 'FSM_test_suites' folder
         for txt_file in os.listdir(fsm_test_suites_path):
             if txt_file.endswith(".txt"):
                 print(f"Found TXT File: {txt_file}")
                 test_suite_files.append(txt_file)
-                # Process the txt file
-                # Command to run the JAR file using the Java Virtual Machine (JVM)
+
                 if aggregation_method is not None:
                     command = ['java', '-jar', jar_file, 'compare',
                                f'{fsm_test_suites_path}/{txt_file}', f'{data_representation}', '-m',
@@ -57,13 +55,10 @@ def calculate_diversity(state_machine_path, data_representation, diversity_metri
                     command = ['java', '-jar', jar_file, 'compare',
                                f'{fsm_test_suites_path}/{txt_file}', f'{data_representation}', '-m',
                                f'{diversity_metric}', '-r', 'RawResults']
-                # Run the command
                 process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-                # Wait for the process to finish and get the return code
                 return_code = process.wait()
 
-                # Print the output and errors
                 output, error = process.communicate()
                 # print("Action:", output.decode())
                 # print("Result:", error.decode())
@@ -72,13 +67,11 @@ def calculate_diversity(state_machine_path, data_representation, diversity_metri
                 diversity_value = result[-2].replace("[", "").replace("]", "")
                 diversity_values.append(diversity_value)
 
-                # Check the return code to see if the process was successful
                 if return_code == 0:
                     print(f"JAR file executed successfully - {data_representation}_{diversity_metric}_{aggregation_method} calculated.")
                 else:
                     print(f"Error: JAR file execution failed with return code {return_code}.")
 
-        # Load the CSV file into a DataFrame
         csv_file_path = f'{state_machine_path}/DC_Report.csv'  # Replace with the actual path to your CSV file
         csv_df = pd.read_csv(csv_file_path)
         if aggregation_method is not None:
@@ -100,33 +93,27 @@ def calculate_diversity(state_machine_path, data_representation, diversity_metri
         print(f"Error: 'FSM_test_suites' folder not found in {state_machine_path}")
 
 
-def calculate_correlation(state_machine_path):
-    csv_file_path = f'{state_machine_path}/DC_Report.csv'  # Replace with the actual path to your CSV file
+def calculate_correlation(csv_file_path):
     csv_df = pd.read_csv(csv_file_path)
 
     diversity_columns = csv_df.iloc[:, 8:50]
     pearson_correlation_coefficients = diversity_columns.apply(
         lambda col: col.astype(float).corr(csv_df['Mutation score All_Mutants OracleOutput'].astype(float)))
-    # Calculate the Spearman rank correlation coefficient
+
     correlation_coefficients = []
     p_values = []
-
-    # Iterate over columns and calculate Spearman correlation
     for col in diversity_columns.columns:
         data_1 = diversity_columns[col].tolist()
         data_2 = csv_df['Mutation score All_Mutants OracleOutput'].tolist()
-        print(data_1, data_2)
         coef, p_value = spearmanr(data_1, data_2)
         correlation_coefficients.append(coef)
         p_values.append(p_value)
-        print(coef, p_value)
 
     csv_df.loc['Pearson Correlation Coefficient'] = ["Pearson Correlation Coefficient", None, None, None, None, None,
                                                      None, None] + pearson_correlation_coefficients.tolist()
     csv_df.loc['Spearman Rank Correlation Coefficients'] = ["Spearman Rank Correlation Coefficients", None, None, None, None, None,
                                                      None, None] + correlation_coefficients
 
-    # Add a row at the bottom of the DataFrame for p-values
     csv_df.loc['P-values'] = ["P-values", None, None, None, None, None,
                                                      None, None] + p_values
     csv_df.to_csv('DC_Report.csv', index=False)
@@ -137,27 +124,29 @@ def calculate_correlation(state_machine_path):
     except Exception as e:
         print(f"Error: {e}")
 
+
 def normalize_data(state_machine_path):
     csv_file_path = f'{state_machine_path}/DC_Report.csv'  # Replace with the actual path to your CSV file
     csv_df = pd.read_csv(csv_file_path)
-    column_1 = csv_df.loc['Mutation score All_Mutants OracleOutput']
-    column_2 = csv_df.loc['EventSequence_Levenshtein_AverageValue']
-    csv_df['normalized_column'] = (column_1 - column_1.min()) / (
-                column_1.max() - column_1.min())
-    print(f'normalized_column \n {csv_df['normalized_column'].tolist()}')
-    print(f'normalized_column \n {csv_df['normalized_column'].tolist()}')
+    diversity_columns = csv_df.iloc[:, 8:50]
+    scaler = MinMaxScaler()
+    normalized_columns = pd.DataFrame(scaler.fit_transform(diversity_columns), columns=diversity_columns.columns)
+    csv_df[diversity_columns.columns] = normalized_columns
+    new_csv_file_path = f'{state_machine_path}/DC_Report_Normalized.csv'
+    try:
+        csv_df.to_csv(new_csv_file_path, index=False)
+        print(f"File '{new_csv_file_path}' updated successfully.")
+    except Exception as e:
+        print(f"Error: {e}")
 
 
 def process_state_machines(root_folder):
-    # Iterate over each folder in the specified root folder
     for state_machine_folder in os.listdir(root_folder):
         state_machine_path = os.path.join(root_folder, state_machine_folder)
 
-        # Check if the item is a directory
         if os.path.isdir(state_machine_path):
             print(f"Processing State Machine Folder: {state_machine_folder}")
 
-            # Call function to create DC_Report.csv for the current State Machine folder
             # create_dc_report(state_machine_path)
 
             data_representations = ["EventSequence", "EventStatePairs", "StateSequence"]
@@ -172,14 +161,12 @@ def process_state_machines(root_folder):
             #         else:
             #             calculate_diversity(state_machine_path, data_representation, diversity_metric, None)
 
-            # calculate_correlation(state_machine_path)
             normalize_data(state_machine_path)
-            break
+            calculate_correlation(f'{state_machine_path}/DC_Report.csv')
+            calculate_correlation(f'{state_machine_path}/DC_Report_Normalized.csv')
 
 
-# Specify the root folder
 state_machines_container_folder = "StateMachines"
 jar_file = 'ComparisonApp.jar'
 
-# Call the function to process folders
 process_state_machines(state_machines_container_folder)
