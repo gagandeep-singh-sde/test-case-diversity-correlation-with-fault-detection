e5059b758f6b61ed53fe4264148fa96cc97fae29
Gagandeep Singh <gagandeep.singh.sde@gmail.com>
2023-12-21 14:32:27 -0500

    checkpoint

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import os
 import subprocess
 
 import pandas as pd
-import numpy as np
+from scipy.stats import spearmanr
 
+import sys
+import numpy
+numpy.set_printoptions(threshold=sys.maxsize)
 
 def create_dc_report(state_machine_folder):
     # Path to TR_Report.csv file in FSM_reports folder
@@ -102,8 +105,30 @@ def calculate_correlation(state_machine_path):
     csv_df = pd.read_csv(csv_file_path)
 
     diversity_columns = csv_df.iloc[:, 8:50]
-    pearson_correlation_coefficients = diversity_columns.apply(lambda col: col.astype(float).corr(csv_df['Mutation score All_Mutants OracleOutput'].astype(float)))
-    csv_df.loc['Pearson Correlation Coefficient'] = ["Pearson Correlation Coefficient", None, None, None, None, None, None, None] + pearson_correlation_coefficients.tolist()
+    pearson_correlation_coefficients = diversity_columns.apply(
+        lambda col: col.astype(float).corr(csv_df['Mutation score All_Mutants OracleOutput'].astype(float)))
+    # Calculate the Spearman rank correlation coefficient
+    correlation_coefficients = []
+    p_values = []
+
+    # Iterate over columns and calculate Spearman correlation
+    for col in diversity_columns.columns:
+        data_1 = diversity_columns[col].tolist()
+        data_2 = csv_df['Mutation score All_Mutants OracleOutput'].tolist()
+        print(data_1, data_2)
+        coef, p_value = spearmanr(data_1, data_2)
+        correlation_coefficients.append(coef)
+        p_values.append(p_value)
+        print(coef, p_value)
+
+    csv_df.loc['Pearson Correlation Coefficient'] = ["Pearson Correlation Coefficient", None, None, None, None, None,
+                                                     None, None] + pearson_correlation_coefficients.tolist()
+    csv_df.loc['Spearman Rank Correlation Coefficients'] = ["Spearman Rank Correlation Coefficients", None, None, None, None, None,
+                                                     None, None] + correlation_coefficients
+
+    # Add a row at the bottom of the DataFrame for p-values
+    csv_df.loc['P-values'] = ["P-values", None, None, None, None, None,
+                                                     None, None] + p_values
     csv_df.to_csv('DC_Report.csv', index=False)
 
     try:
@@ -111,6 +136,16 @@ def calculate_correlation(state_machine_path):
         print(f"File '{csv_file_path}' updated successfully.")
     except Exception as e:
         print(f"Error: {e}")
+
+def normalize_data(state_machine_path):
+    csv_file_path = f'{state_machine_path}/DC_Report.csv'  # Replace with the actual path to your CSV file
+    csv_df = pd.read_csv(csv_file_path)
+    column_1 = csv_df.loc['Mutation score All_Mutants OracleOutput']
+    column_2 = csv_df.loc['EventSequence_Levenshtein_AverageValue']
+    csv_df['normalized_column'] = (column_1 - column_1.min()) / (
+                column_1.max() - column_1.min())
+    print(f'normalized_column \n {csv_df['normalized_column'].tolist()}')
+    print(f'normalized_column \n {csv_df['normalized_column'].tolist()}')
 
 
 def process_state_machines(root_folder):
@@ -123,21 +158,23 @@ def process_state_machines(root_folder):
             print(f"Processing State Machine Folder: {state_machine_folder}")
 
             # Call function to create DC_Report.csv for the current State Machine folder
-            create_dc_report(state_machine_path)
+            # create_dc_report(state_machine_path)
 
-            data_representations = ["EventSequence", "EventPairs", "StateSequence"]
+            data_representations = ["EventSequence", "EventStatePairs", "StateSequence"]
             diversity_metrics = ["Levenshtein", "SimpsonDiversity", "Hamming", "ShannonIndex", "Nei"]
             aggregation_methods = ["AverageValue", "SquaredSummation", "Manhattan", "Euclidean"]
-            for data_representation in data_representations:
-                for diversity_metric in diversity_metrics:
-                    if diversity_metric != "ShannonIndex" and diversity_metric != "Nei":
-                        for aggregation_method in aggregation_methods:
-                            calculate_diversity(state_machine_path, data_representation, diversity_metric,
-                                                aggregation_method)
-                    else:
-                        calculate_diversity(state_machine_path, data_representation, diversity_metric, None)
+            # for data_representation in data_representations:
+            #     for diversity_metric in diversity_metrics:
+            #         if diversity_metric != "ShannonIndex" and diversity_metric != "Nei":
+            #             for aggregation_method in aggregation_methods:
+            #                 calculate_diversity(state_machine_path, data_representation, diversity_metric,
+            #                                     aggregation_method)
+            #         else:
+            #             calculate_diversity(state_machine_path, data_representation, diversity_metric, None)
 
-            calculate_correlation(state_machine_path)
+            # calculate_correlation(state_machine_path)
+            normalize_data(state_machine_path)
+            break
 
 
 # Specify the root folder
